e34173aa8948286fb18d0b8dcee48d1231f81a73
DiegoNunes <diego@prodwin.com.br>
2018-05-01 21:47:37 -0300

    Exemplo do post DefaultDictionary

---
+++ DefaultDictionary/DefaultDictionary/DefaultDictionary.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+
+namespace DiegoNunesCom {
+	public class DefaultDictionary<TKey, TValue>: Dictionary<TKey, TValue> {
+		public new TValue this[TKey key] {
+			get {
+				if(ContainsKey(key)) {
+					return base[key];
+				}
+				return default(TValue);
+			}
+			set {
+				base[key] = value;
+			}
+		}
+	}
+}
---
+++ DefaultDictionary/DefaultDictionary/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+
+namespace DiegoNunesCom {
+	class Program {
+		static void Main(string[] args) {
+			DefaultDictionary<string, string> parametros = new DefaultDictionary<string, string>();
+
+			parametros["usuario"] = "diego@diego-nunes.com";
+
+			Console.WriteLine("Usuario: " + parametros["usuario"]);
+			Console.WriteLine("Senha  : " + parametros["senha"]);
+
+			Console.ReadKey();
+		}
+	}
+}
